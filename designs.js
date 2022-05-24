30f9f7296251f7aef4298673badf80102cb5e5e9
mrehan0000 <98801861+mrehan0000@users.noreply.github.com>
2022-05-24 13:12:23 +0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,27 +6,26 @@ const color = document.getElementById('colorPicker');
 const table = document.getElementById('pixelCanvas');
 
 //enabling submit functionality
-sizePicker.addEventListener('submit',function(e){
-    e.preventDefault();
-    var row =  myrow.value;
-    var column = mycolumn.value;
-    table.innerHTML= '';
-    makeGrid(row,column);
+sizePicker.addEventListener('submit', function(e) {
+	e.preventDefault();
+	var row = myrow.value;
+	var column = mycolumn.value;
+	table.innerHTML = '';
+	makeGrid(row, column);
 });
 
 //making the grid as per user input
-function makeGrid(row, column){
-    for (var r=0; r<row; r++){
-        let mytable = table.insertRow();
-        for (var c=0; c<column; c++){
-        let cell = mytable.insertCell();
+function makeGrid(row, column) {
+	for (var r = 0; r < row; r++) {
+		let mytable = table.insertRow();
+		for (var c = 0; c < column; c++) {
+			let cell = mytable.insertCell();
 
-        //add the color changing functionality to the cells
-        cell.addEventListener('click',function(event){
-        let chosenColor = color.value;
-        event.target.style.backgroundColor = chosenColor;
-    })
-        };
-    };
+			//add the color changing functionality to the cells
+			cell.addEventListener('click', function(event) {
+				let chosenColor = color.value;
+				event.target.style.backgroundColor = chosenColor;
+			})
+		};
+	};
 };
-
